0aefa3f467aeee41ccbcd7cfdff773647f42c5fb
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2023-03-09 15:07:38 -0500

    Merge remote-tracking branch 'origin/carlos-practice' into Will-practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import AdminDashboard from './components/AdminDashboard';
+import DashboardPage from './Pages/DashboardPage';
+import LoginPage from './Pages/LoginPage';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-   <AdminDashboard/>
-  );
+    <Router>
+      <Routes>
+        <Route path ='/' element={<LoginPage/>} />
+        <Route path ='/dashboard' element={<DashboardPage/>} />
+      </Routes>
+    </Router>
+  );  
 }
 
 export default App;
